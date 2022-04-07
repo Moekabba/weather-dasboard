09f29ce4bbea4d706d36573a163add4ec35b1a19
Mahamadou Kabba <mkabba34@gmail.com>
2022-04-06 20:37:47 -0400

    save here just incase

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,13 @@
 // my Api key to be use
-var myApi = "ecdc2e3e10911d9dd68cf81fb00dced1";
+var myApiKey = "ecdc2e3e10911d9dd68cf81fb00dced1";
+var lat = 51.5073219;
+var lot = -0.1276474;
 // api with uv but can't accept q as location
 // https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude&exclude=hourly,minutely,alerts&appid=ecdc2e3e10911d9dd68cf81fb00dced1
 // Dom Elements
 var searchButton = document.getElementById("search-btn");
+var formEl = document.getElementById("form");
+var cityInputEL = document.getElementById("input");
 var searchHistory = document.querySelector("ul");
 var cityEl = document.getElementById("city");
 var tempEl = document.getElementById("temp");
@@ -20,11 +24,15 @@ $("#currentDay").text(today);
 //api.openweathermap.org/data/2.5/onecall?q=london&exclude=ecdc2e3e10911d9dd68cf81fb00dced1
 // get/ calling Api function
 // http: https:
-function getApi() {
-  searchHistory.innerHTML = " ";
+function getApi(name) {
+  // searchHistory.innerHTML = " ";
   // console.log("HELLO");
   var requestUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=senegal&cnt=5&units=metric&appid=ecdc2e3e10911d9dd68cf81fb00dced1";
+    // " https://api.openweathermap.org/data/2.5/onecall?lat=51.5073219&lon=-0.1276474&units=metric&exclude=minutely,hourly&appid=ecdc2e3e10911d9dd68cf81fb00dced1";
+
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    name +
+    "&units=metric&appid=ecdc2e3e10911d9dd68cf81fb00dced1";
 
   fetch(requestUrl)
     .then(function (response) {
@@ -32,36 +40,32 @@ function getApi() {
       return response.json();
     })
     .then(function (data) {
+      // I want to use an HTTP request's response to display data to the user.
+
       cityEl.textContent = " Weather in  " + data.name;
-      console.log(data);
       tempEl.textContent = data.main.temp + " °C";
       iconEl.src = "http://openweathermap.org/img/wn/03n@2x.png";
       discEl.textContent = data.weather[0].description;
       humidEl.textContent = "humidity is: % " + data.main.humidity;
       windEl.textContent = "wind speed of " + data.wind.speed + " km/h";
       timeEl.textContent = "time zone: " + data.timezone;
-      return data;
-
       //  uviEl.innerHTML =
-      // console.log(data);
-      // creating list of search histry
-      // for (var i = 0; i < data.length; i++) {
-      //   // Create a list element
-      //   var historyList = document.createElement("li");
 
-      //   // Set the text of the list element to the JSON response's .html_url property
-      //   // historyList.textContent = data[data].city;
-
-      //   // Append the li element to the id associated with the ul element.
-      //   searchHistory.appendChild(historyList);
-      // }
+      return data;
     });
-  function searchInput() {
-    this.getApi(searchButton.value);
-  }
 }
-// search: function () {
-//   this.fetchWeather(document.querySelector(".search-bar").value);
-// }
 
-searchButton.addEventListener("click", getApi);
+//  I want to collect user input to form HTTP requests.
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var cityname = cityInputEL.value.trim();
+  if (cityname) {
+    getApi(cityname);
+    cityInputEL.value = "";
+  } else {
+    alert("Please enter a city name");
+    console.log(event);
+  }
+};
+
+formEl.addEventListener("submit", formSubmitHandler);
